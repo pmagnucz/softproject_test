fb7c0d2660ce99795e380c46fe100f68bed52a02
Magnucz, Peter <Peter.Magnucz@misys.com>
2017-09-25 13:31:33 +0200

    Vehicle and Car model added

---
+++ src/main/java/uni/miskolc/iit/model/Car.java
@@ -0,0 +1,67 @@
+package uni.miskolc.iit.model;
+
+/**
+ * Created by pmagnucz on 2017. 09. 25..
+ */
+public class Car extends Vehicle {
+    private String plateNumber;
+    private String vehicleIdentificationNumber;
+    private boolean drawBar;
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getVehicleIdentificationNumber() {
+        return vehicleIdentificationNumber;
+    }
+
+    public void setVehicleIdentificationNumber(String vehicleIdentificationNumber) {
+        this.vehicleIdentificationNumber = vehicleIdentificationNumber;
+    }
+
+    public boolean isDrawBar() {
+        return drawBar;
+    }
+
+    public void setDrawBar(boolean drawBar) {
+        this.drawBar = drawBar;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Car car = (Car) o;
+
+        if (isDrawBar() != car.isDrawBar()) return false;
+        if (getPlateNumber() != null ? !getPlateNumber().equals(car.getPlateNumber()) : car.getPlateNumber() != null)
+            return false;
+        return getVehicleIdentificationNumber() != null ? getVehicleIdentificationNumber().equals(car.getVehicleIdentificationNumber()) : car.getVehicleIdentificationNumber() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (getPlateNumber() != null ? getPlateNumber().hashCode() : 0);
+        result = 31 * result + (getVehicleIdentificationNumber() != null ? getVehicleIdentificationNumber().hashCode() : 0);
+        result = 31 * result + (isDrawBar() ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "plateNumber='" + plateNumber + '\'' +
+                ", vehicleIdentificationNumber='" + vehicleIdentificationNumber + '\'' +
+                ", drawBar=" + drawBar +
+                '}';
+    }
+}
