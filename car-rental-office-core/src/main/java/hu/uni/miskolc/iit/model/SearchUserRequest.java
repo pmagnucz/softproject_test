0f6ff252f326eb9c95674a32db644ed459873ecd
Rozgonyi László <32123665+rozgonyilaszlo@users.noreply.github.com>
2017-09-27 10:53:52 +0200

    Added UserManagementService.

---
+++ car-rental-office-core/src/main/java/hu/uni/miskolc/iit/model/SearchUserRequest.java
@@ -0,0 +1,43 @@
+package hu.uni.miskolc.iit.model;
+
+/**
+ * Created by rozgonyi on 2017. 09. 27..
+ */
+public class SearchUserRequest {
+    private String userName;
+    private long phoneNumber;
+    private String address;
+    private String drivingLicenceNumber;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDrivingLicenceNumber() {
+        return drivingLicenceNumber;
+    }
+
+    public void setDrivingLicenceNumber(String drivingLicenceNumber) {
+        this.drivingLicenceNumber = drivingLicenceNumber;
+    }
+}
---
+++ car-rental-office-core/src/main/java/hu/uni/miskolc/iit/service/UserManagementService.java
@@ -0,0 +1,50 @@
+package hu.uni.miskolc.iit.service;
+
+import hu.uni.miskolc.iit.model.SearchUserRequest;
+import hu.uni.miskolc.iit.model.User;
+
+import java.util.List;
+
+
+/**
+ * Created by rozgonyi on 2017. 09. 27..
+ */
+public interface UserManagementService {
+    /**
+     * Létrehoz egy új felhasználót.
+     * @param user
+     * @return User object
+     * */
+    User createNewUser(User user);
+    /**
+     * A paraméterben megadott id-jú felhasználóval tér vissza.
+     * @param id
+     * @return User object
+     * */
+    User getUserById(int id);
+
+    /**
+     * A szűrési paramétereknek megfelelő felhasználót adja vissza.
+     * @param searchUserRequest filter options ()
+     * @return List of User objects
+     * */
+    List<User> getUserByFilterOptions(SearchUserRequest searchUserRequest);
+
+    /**
+     * Az összes felhasználót vissza adja.
+     * @return List of User objects
+     * */
+    List<User> getUsers();
+
+    /**
+     * Egy felhasználó tulajdonságait módosítja.
+     * @param user
+     * @return User object
+     * */
+    User updateUser(User user);
+
+    /**Törli a paraméterben kapott felhasználót.
+     * @param user
+     */
+    void deleteUser(User user);
+}
