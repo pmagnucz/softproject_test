c50a61037db1b704d867924f6a3cf75c83dcb6d6
rendzso <bodnarreno@gmail.com>
2017-10-04 12:59:07 +0200

    UserManagementServiceImplTest empty added

--- car-rental-office-service/src/test/java/hu.uni.miskolc.iit/UserManagementServiceImplTest.java
+++ car-rental-office-service/src/test/java/hu.uni.miskolc.iit/UserManagementServiceImplTest.java
@@ -1,5 +1,12 @@
 package hu.uni.miskolc.iit;
 
+import hu.uni.miskolc.iit.repositories.UserRepository;
+import hu.uni.miskolc.iit.service.UserManagementService;
+
+
 public class UserManagementServiceImplTest {
-    
+    private UserManagementService userManagementService;
+    private UserRepository userRepository;
+
+    }
 }
