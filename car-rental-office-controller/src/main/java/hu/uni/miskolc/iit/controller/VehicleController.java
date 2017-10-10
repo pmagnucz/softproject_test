5cb99f6bbc747e8c642a1563dc0fc318612845dc
rendzso <bodnarreno@gmail.com>
2017-10-10 19:17:50 +0200

    Vehicle controller added

--- car-rental-office-controller/src/main/java/hu/uni/miskolc/iit/controller/VehicleController.java
+++ car-rental-office-controller/src/main/java/hu/uni/miskolc/iit/controller/VehicleController.java
@@ -7,9 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * Created by botty on 2017. 10. 10..
  */
-public class VehicleController {
 
+@RestController
+@RequestMapping("/vehicle")
+
+
+public class VehicleController {
+    private VehicleManagementService vehicleManagementService;
+
+    @Autowired
+    public void setVehicleManagementService(VehicleManagementService vehicleManagementService) {
+        this.vehicleManagementService = vehicleManagementService;
+    }
+
+    @PostMapping(value = "/search")
+    public ResponseEntity<List<Vehicle>> getVehicleByFilterOptions(@RequestBody SearchVehicleRequest searchVehicleRequest) {
+        List<Vehicle> vehicles = vehicleManagementService.getVehicleByFilterOptions(searchVehicleRequest);
+        return ResponseEntity.ok(vehicles);
+    }
 }
+
