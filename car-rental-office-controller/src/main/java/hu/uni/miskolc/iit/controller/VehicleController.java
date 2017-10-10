ceab2f25bbe815d488b145bb3ff5d0873662a8e7
bottyan94 <31881743+bottyan94@users.noreply.github.com>
2017-10-10 19:03:35 +0200

    Controller: VehicleController Added(empty)

--- car-rental-office-controller/src/main/java/hu/uni/miskolc/iit/controller/VehicleController.java
+++ car-rental-office-controller/src/main/java/hu/uni/miskolc/iit/controller/VehicleController.java
@@ -1,7 +1,15 @@
 package hu.uni.miskolc.iit.controller;
+import hu.uni.miskolc.iit.model.SearchVehicleRequest;
+import hu.uni.miskolc.iit.model.Vehicle;
+import hu.uni.miskolc.iit.service.VehicleManagementService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by botty on 2017. 10. 10..
  */
 public class VehicleController {
+
 }
