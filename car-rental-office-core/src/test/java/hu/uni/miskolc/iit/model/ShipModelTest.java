adf95a765ce0683d0426f1965dd03dfc7273219a
rendzso <bodnarreno@gmail.com>
2017-10-16 21:24:37 +0200

    ShipModelTest added

---
+++ car-rental-office-core/src/test/java/hu/uni/miskolc/iit/model/ShipModelTest.java
@@ -0,0 +1,47 @@
+package hu.uni.miskolc.iit.model;
+
+import static org.junit.Assert.*;
+
+import hu.uni.miskolc.iit.model.Ship;
+import org.junit.*;
+
+import java.lang.Exception;
+import java.lang.String;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ShipModelTest {
+    private Ship ship;
+
+    @Before
+    public void setUp() throws Exception{
+        this.ship = new Ship("Cunami", 10000, TRUE);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getShipID() throws Exception{
+        String expected = "Cunami";
+        String actual = ship.getShipId();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getLenght() throws Exception {
+        double expected = 10000;
+        double actual = vehicle.getLenght();
+        assertEquals(expected, actual, 0.1);
+    }
+
+    @Test
+    public void isWithTrailer() throws Exception{
+        boolean expected = true;
+        boolean actual = ship.isWithTrailer();
+        assertEquals(expected, actual);
+    }
+
+}
