0b790b576ea62e74d399a838c6249a8dcba9cba2
Rozgonyi László <32123665+rozgonyilaszlo@users.noreply.github.com>
2017-10-17 21:27:01 +0200

    Vehicle controller and some minor changes.

--- car-rental-office-controller/src/main/java/hu/uni/miskolc/iit/controller/VehicleController.java
+++ car-rental-office-controller/src/main/java/hu/uni/miskolc/iit/controller/VehicleController.java
@@ -13,10 +13,8 @@ import java.util.List;
  * Created by botty on 2017. 10. 10..
  */
 
-/*@RestController
-@RequestMapping("/vehicle")*/
-
-
+@RestController
+@RequestMapping("/vehicle")
 public class VehicleController {
     private VehicleManagementService vehicleManagementService;
 
@@ -25,10 +23,40 @@ public class VehicleController {
         this.vehicleManagementService = vehicleManagementService;
     }
 
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addNewVehice(@RequestBody Vehicle vehicle){
+        Vehicle returned = vehicleManagementService.addNewVehicle(vehicle);
+        return returned.toString() + "car added.";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String getVehicleById(@PathVariable int id){
+        Vehicle vehicle = vehicleManagementService.getVehicleById(id);
+        return vehicle.toString();
+    }
+
     @PostMapping(value = "/search")
     public ResponseEntity<List<Vehicle>> getVehicleByFilterOptions(@RequestBody SearchVehicleRequest searchVehicleRequest) {
         List<Vehicle> vehicles = vehicleManagementService.getVehicleByFilterOptions(searchVehicleRequest);
         return ResponseEntity.ok(vehicles);
     }
-}
 
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public String getVehicles(){
+        List<Vehicle> vehicles = vehicleManagementService.getVehicles();
+        return vehicles.toString();
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String updateVehicle(@RequestBody Vehicle vehicle){
+        Vehicle returned = vehicleManagementService.updateVehicle(vehicle);
+        return vehicle.toString() + "changed to: " + returned.toString();
+    }
+
+    /*itt 200-as kellene majd küldeni, ha jó lesz, ha nem valami hibát*/
+    @RequestMapping(value = "/remove", method = RequestMethod.POST)
+    public String removeVehicle(@RequestBody Vehicle vehicle){
+        vehicleManagementService.removeVehicle(vehicle);
+        return "Lefutott.";
+    }
+}
