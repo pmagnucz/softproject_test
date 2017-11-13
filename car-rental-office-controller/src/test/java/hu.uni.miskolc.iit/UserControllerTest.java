36b7dfd6ed8a0ba5378d853ad624dbe7eb0c25a9
Rozgonyi László <32123665+rozgonyilaszlo@users.noreply.github.com>
2017-11-13 20:21:57 +0100

    User management controller test init

---
+++ car-rental-office-controller/src/test/java/hu.uni.miskolc.iit/UserControllerTest.java
@@ -0,0 +1,47 @@
+package hu.uni.miskolc.iit;
+
+/**
+ * Created by rozgonyi on 2017.11.13..
+ */
+
+public class RentControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void createUser() throws Exception {
+
+    }
+
+    @Test
+    public void getAllUser() throws Exception {
+
+    }
+
+    @Test
+    public void updateUser() throws Exception {
+
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+
+    }
+
+    @Test
+    public void getUserById() throws Exception {
+
+    }
+
+    @Test
+    public void getUserByFilterOptions() throws Exception {
+
+    }
+}
