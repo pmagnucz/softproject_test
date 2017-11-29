2c8260538304ee525c079e117f0e353aa1f80c85
tdavid90 <32302279+tdavid90@users.noreply.github.com>
2017-11-29 12:27:57 +0100

    getRentCount integration updated

--- car-rental-office-application/src/test/java/hu/uni/miskolc/iit/RentManagementIT.java
+++ car-rental-office-application/src/test/java/hu/uni/miskolc/iit/RentManagementIT.java
@@ -218,6 +218,13 @@ public class RentManagementIT {
 
     @Test
     public void getRentCount() throws Exception {
+        rentController.createRent(rent);
+        rent.setId(10L);
+        rentController.createRent(rent);
+
+        int count = rentController.getRentCount().getBody();
+        Assert.assertEquals(2,count);
+
     }
 
     @Test(expected = NegativeValueException.class)
