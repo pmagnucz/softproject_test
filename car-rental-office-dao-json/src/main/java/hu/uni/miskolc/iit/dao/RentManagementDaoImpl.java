4cc1141ecaaee6eb278581a4a4dc5082a11e2423
Magnucz, Peter <Peter.Magnucz@wup.hu>
2017-12-05 01:30:51 +0100

    DAO-JSON module added

---
+++ car-rental-office-dao-json/src/main/java/hu/uni/miskolc/iit/dao/RentManagementDaoImpl.java
@@ -0,0 +1,151 @@
+package hu.uni.miskolc.iit.dao;
+import java.util.Date;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.sun.org.apache.regexp.internal.RE;
+import hu.uni.miskolc.iit.dao.beans.RentDaoBean;
+import hu.uni.miskolc.iit.model.Rent;
+import hu.uni.miskolc.iit.model.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by pmagnucz on 2017. 12. 03..
+ */
+public class RentManagementDaoImpl implements RentManagementDao {
+    private File database;
+
+    public RentManagementDaoImpl(File file)
+    {
+        this.database = file;
+    }
+
+    @Override
+    public Rent createRent(Rent rent) {
+        boolean exist = false;
+        List<Rent> rents = readDatabase();
+        if (rents.size() != 0){
+            for (Rent r : rents)
+            {
+                if (r.getId() == rent.getId())
+                {
+                    exist = true;
+                    r.setCustomerId(rent.getCustomerId());
+                    r.setCompanyId(rent.getCompanyId());
+                    r.setVehicleId(rent.getVehicleId());
+                    r.setStartDate(rent.getStartDate());
+                    r.setEndDate(rent.getEndDate());
+                    r.setDurationExtendable(rent.isDurationExtendable());
+                    r.setExtendedHours(rent.getExtendedHours());
+                    r.setKmUsed(rent.getKmUsed());
+                    r.setDayFee(rent.getDayFee());
+                    r.setKmFee(rent.getKmFee());
+                    r.setOtherFee(rent.getOtherFee());
+                    r.setTotalFee(rent.getTotalFee());
+                    r.setPaid(rent.isPaid());
+                }
+            }
+        }
+        if (!exist)
+        {
+            rents.add(rent);
+        }
+
+        writeDatabase(rents);
+        return rent;
+    }
+
+    @Override
+    public Rent getRentById(Long id) {
+        List<Rent> rents = readDatabase();
+        for (Rent rent : rents)
+        {
+            if (rent.getId().equals(id))
+            {
+                return rent;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<Rent> getRents() {
+        return readDatabase();
+    }
+
+    @Override
+    public void deleteRent(Rent rent) {
+        List<Rent> rents = readDatabase();
+        rents.remove(rent);
+        writeDatabase(rents);
+    }
+
+    @Override
+    public boolean exists(Rent rent) {
+        List<Rent> rents = readDatabase();
+        return rents.contains(rent);
+    }
+
+    @Override
+    public void clear() {
+        writeDatabase(new ArrayList<Rent>());
+    }
+
+    private List<Rent> readDatabase(){
+        List<Rent> result = new ArrayList<Rent>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            List<RentDaoBean> beans = Arrays.asList(mapper.readValue(database, RentDaoBean[].class));
+            for (RentDaoBean bean : beans)
+            {
+                result.add(bean.extract());
+            }
+        } catch (IOException e) {
+            System.out.println(database.getAbsolutePath());
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private void writeDatabase(List<Rent> rents)
+    {
+        List<RentDaoBean> beans = new ArrayList<RentDaoBean>();
+        for (Rent rent : rents){
+            beans.add(convert(rent));
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(database, beans);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private RentDaoBean convert(Rent rent)
+    {
+        RentDaoBean bean = new RentDaoBean();
+        bean.setId(rent.getId());
+        bean.setCustomerId(rent.getCustomerId());
+        bean.setCompanyId(rent.getCompanyId());
+        bean.setVehicleId(rent.getVehicleId());
+        bean.setStartDate(rent.getStartDate());
+        bean.setEndDate(rent.getEndDate());
+        bean.setDurationExtendable(rent.isDurationExtendable());
+        bean.setExtendedHours(rent.getExtendedHours());
+        bean.setKmUsed(rent.getKmUsed());
+        bean.setDayFee(rent.getDayFee());
+        bean.setKmFee(rent.getKmFee());
+        bean.setOtherFee(rent.getOtherFee());
+        bean.setTotalFee(rent.getTotalFee());
+        bean.setPaid(rent.isPaid());
+
+        return bean;
+    }
+}
---
+++ car-rental-office-dao-json/src/main/java/hu/uni/miskolc/iit/dao/beans/RentDaoBean.java
@@ -0,0 +1,206 @@
+package hu.uni.miskolc.iit.dao.beans;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import hu.uni.miskolc.iit.model.Rent;
+import java.util.Date;
+
+/**
+ * Created by pmagnucz on 2017. 12. 04..
+ */
+public class RentDaoBean {
+    private Long id;
+
+    private Long customerId;
+    private Long companyId;
+    private Long vehicleId;
+
+    @JsonFormat(pattern="yyyy-MM-dd", timezone = "Europe/Budapest")
+    private Date startDate;
+
+    @JsonFormat(pattern="yyyy-MM-dd", timezone = "Europe/Budapest")
+    private Date endDate;
+
+    private boolean durationExtendable;
+    private int extendedHours;
+    private int kmUsed;
+    private double dayFee;
+    private double kmFee;
+    private double otherFee;
+    private double totalFee;
+    private boolean paid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isDurationExtendable() {
+        return durationExtendable;
+    }
+
+    public void setDurationExtendable(boolean durationExtendable) {
+        this.durationExtendable = durationExtendable;
+    }
+
+    public int getExtendedHours() {
+        return extendedHours;
+    }
+
+    public void setExtendedHours(int extendedHours) {
+        this.extendedHours = extendedHours;
+    }
+
+    public int getKmUsed() {
+        return kmUsed;
+    }
+
+    public void setKmUsed(int kmUsed) {
+        this.kmUsed = kmUsed;
+    }
+
+    public double getDayFee() {
+        return dayFee;
+    }
+
+    public void setDayFee(double dayFee) {
+        this.dayFee = dayFee;
+    }
+
+    public double getKmFee() {
+        return kmFee;
+    }
+
+    public void setKmFee(double kmFee) {
+        this.kmFee = kmFee;
+    }
+
+    public double getOtherFee() {
+        return otherFee;
+    }
+
+    public void setOtherFee(double otherFee) {
+        this.otherFee = otherFee;
+    }
+
+    public double getTotalFee() {
+        return totalFee;
+    }
+
+    public void setTotalFee(double totalFee) {
+        this.totalFee = totalFee;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RentDaoBean rent = (RentDaoBean) o;
+
+        if (id != rent.id) return false;
+        if (customerId != rent.customerId) return false;
+        if (companyId != rent.companyId) return false;
+        if (vehicleId != rent.vehicleId) return false;
+        if (durationExtendable != rent.durationExtendable) return false;
+        if (extendedHours != rent.extendedHours) return false;
+        if (kmUsed != rent.kmUsed) return false;
+        if (Double.compare(rent.dayFee, dayFee) != 0) return false;
+        if (Double.compare(rent.kmFee, kmFee) != 0) return false;
+        if (Double.compare(rent.otherFee, otherFee) != 0) return false;
+        if (Double.compare(rent.totalFee, totalFee) != 0) return false;
+        if (paid != rent.paid) return false;
+        if (!startDate.equals(rent.startDate)) return false;
+        return endDate.equals(rent.endDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Rent{" +
+                "id=" + id +
+                ", customerId=" + customerId +
+                ", companyId=" + companyId +
+                ", vehicleId=" + vehicleId +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", durationExtendable=" + durationExtendable +
+                ", extendedHours=" + extendedHours +
+                ", kmUsed=" + kmUsed +
+                ", dayFee=" + dayFee +
+                ", kmFee=" + kmFee +
+                ", otherFee=" + otherFee +
+                ", totalFee=" + totalFee +
+                ", paid=" + paid +
+                '}';
+    }
+
+    public Rent extract()
+    {
+        Rent rent= new Rent();
+        rent.setId(this.getId());
+        rent.setCustomerId(this.getCustomerId());
+        rent.setCompanyId(this.getCompanyId());
+        rent.setVehicleId(this.getVehicleId());
+        rent.setStartDate(this.getStartDate());
+        rent.setEndDate(this.getEndDate());
+        rent.setDurationExtendable(this.durationExtendable);
+        rent.setExtendedHours(this.getExtendedHours());
+        rent.setKmUsed(this.getKmUsed());
+        rent.setDayFee(this.getDayFee());
+        rent.setKmFee(this.getKmFee());
+        rent.setOtherFee(this.getOtherFee());
+        rent.setTotalFee(this.getTotalFee());
+        rent.setPaid(this.isPaid());
+        return rent;
+    }
+}
