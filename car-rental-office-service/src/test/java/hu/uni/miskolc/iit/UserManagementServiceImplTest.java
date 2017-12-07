4659c0bb88ab320eff6b612128f724ef9709f033
tdavid90 <32302279+tdavid90@users.noreply.github.com>
2017-12-07 11:21:44 +0100

    Update UserManagementServiceImplTest.java

--- car-rental-office-service/src/test/java/hu/uni/miskolc/iit/UserManagementServiceImplTest.java
+++ car-rental-office-service/src/test/java/hu/uni/miskolc/iit/UserManagementServiceImplTest.java
@@ -3,23 +3,47 @@ package hu.uni.miskolc.iit;
 import hu.uni.miskolc.iit.dao.UserManagementDao;
 import hu.uni.miskolc.iit.dao.UserManagementDaoImpl;
 import hu.uni.miskolc.iit.model.Customer;
+import hu.uni.miskolc.iit.model.Company;
 import hu.uni.miskolc.iit.model.User;
 import hu.uni.miskolc.iit.service.UserManagementService;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 import java.io.File;
 
 
 public class UserManagementServiceImplTest {
     private UserManagementService userManagementService;
+   
 // TODO UserManagementServiceImplTest
     @Before
     public void before()
     {
         UserManagementDao userManagementDao = new UserManagementDaoImpl(new File("src/test/resources/database.json"));
         userManagementService = new UserManagementServiceImpl(userManagementDao);
+       
+        
+        user = new User();
+        customer = new Customer();
+        
+        
+        user.setId(1L);
+        user.setPhoneNumber("+363231231231");
+        user.setAddress("Miskolc");
+        user.setUserName("Jóska István");
+        
+        customer.setId(1L);
+        customer.setPhoneNumber("+363231231231");
+        customer.setAddress("Miskolc");
+        customer.setUserName("Jóska István");
+        customer.setUserId(1L);
+        customer.setYearOfBirth(1990);
+        customer.setDrivingLincenceNumber("21213565");
     }
 
     @Test
